eca59bb3be0cdf7156d2ebcf7fdf940fa0892520
Matheus Rennan <matheusrennan70@gmail.com>
2026-06-19 11:02:22 -0300

    teste build erro

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -2,6 +2,6 @@ var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 
 var app = builder.Build(); 
 
-app.MapGet("/", () => "Pipeline funcionando! Teste em develop novamente"); 
+app.MapGet("/", () => "Pipeline funcionando! Teste em develop novamente")
 
 app.Run(); 
