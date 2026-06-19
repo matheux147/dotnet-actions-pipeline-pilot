790be1278451c37f5439fa93aaeeb2e701e4d716
Matheus Rennan <matheusrennan70@gmail.com>
2026-06-18 21:03:51 -0300

    feat: test develop pipeline run

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -2,6 +2,6 @@ var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 
 var app = builder.Build(); 
 
-app.MapGet("/", () => "Pipeline funcionando!"); 
+app.MapGet("/", () => "Pipeline funcionando! Teste em develop"); 
 
 app.Run(); 
